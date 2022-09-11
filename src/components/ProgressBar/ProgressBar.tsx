a5aeab7782e8f911971313c73f703f740effc5fe
Мария Романович <mariagalko.info@gmail.com>
2022-09-11 14:00:01 +0300

    Refactoring ProgressBar component

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -8,46 +8,18 @@ type ProgressProps = {
 export default function ProgressBar({ completed, isStart }: ProgressProps) {
   return (
     <div className="progress">
-      {/* {completed === null ? (
-        <div className="progress__inner progress__inner_start">
-          <div className="progress__filler progress__filler_start">
-            <span className="progress__label progress__label_start">
-              Набери 10 правильных ответов подряд, чтобы победить!
-            </span>
+      <div className="progress__inner">
+        {isStart && (
+          <div className="progress__label progress__label_start">
+            Набери 10 правильных ответов подряд, чтобы победить!
           </div>
-        </div>
-      ) : (
-        <div className="progress__inner">
-          <div style={{ width: `${completed}%` }} className="progress__filler">
+        )}
+        <div style={{ width: `${completed}%` }} className="progress__filler">
+          {completed !== 0 && (
             <span className="progress__label">{completed}%</span>
-          </div>
-          )
-        </div>
-      )} */}
-
-      {isStart ? (
-        <div className="progress__inner progress__inner_start">
-          <div className="progress__filler progress__filler_start">
-            <span className="progress__label progress__label_start">
-              Набери 10 правильных ответов подряд, чтобы победить!
-            </span>
-          </div>
-        </div>
-      ) : (
-        <div className="progress__inner">
-          {completed === 0 ? (
-            <div style={{ width: `0%` }} className="progress__filler">
-              {/* <span className="progress__label">{completed}%</span> */}
-            </div>
-          ) : (
-            <div
-              style={{ width: `${completed}%` }}
-              className="progress__filler">
-              <span className="progress__label">{completed}%</span>
-            </div>
           )}
         </div>
-      )}
+      </div>
     </div>
   );
 }
