c351dc0ba745200bafd8a5ed1594caf533fbeb79
Мария Романович <mariagalko.info@gmail.com>
2022-09-19 22:21:56 +0300

    Change social icons to React Component style

--- src/components/FinishPage/FinishPage.tsx
+++ src/components/FinishPage/FinishPage.tsx
@@ -1,12 +1,19 @@
 // import { useEffect, useRef } from "react";
 import "./FinishPage.scss";
-import Telegram from "../../assets/images/telegram.svg";
-import VK from "../../assets/images/vk.svg";
-import Whatsapp from "../../assets/images/whatsapp.svg";
-import Facebook from "../../assets/images/facebook.svg";
-import OK from "../../assets/images/ok.svg";
-import Twitter from "../../assets/images/twitter.svg";
+// import Telegram from "../../assets/images/telegram.svg";
+// import VK from "../../assets/images/vk.svg";
+// import Whatsapp from "../../assets/images/whatsapp.svg";
+// import Facebook from "../../assets/images/facebook.svg";
+// import OK from "../../assets/images/ok.svg";
+// import Twitter from "../../assets/images/twitter.svg";
 import { Link } from "react-router-dom";
+import { ReactComponent as TelegramIcon} from "../../assets/images/telegram.svg";
+import { ReactComponent as VkIcon} from "../../assets/images/vk.svg";
+import { ReactComponent as WhatsappIcon} from "../../assets/images/whatsapp.svg";
+import { ReactComponent as FacebookIcon} from "../../assets/images/facebook.svg";
+import { ReactComponent as OkIcon} from "../../assets/images/ok.svg";
+import { ReactComponent as TwitterIcon} from "../../assets/images/twitter.svg";
+// import { ReactComponent as VkIcon} from "../../assets/images/vk.svg";;
 
 export default function FinishPage() {
   let message = "Я отгадал 10 фильмов подряд на loveyoucinema.com";
@@ -22,45 +29,37 @@ export default function FinishPage() {
           href={`http://www.facebook.com/sharer.php?u=loveyoucinema.com&t=Я отгадал 10 фильмов подряд`}
           target="_blank"
           rel="noreferrer">
-          <img alt="Facebook" src={Facebook} className="finishPage__icon"/>
-          {/* <svg className="finishPage__icon"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1024 1024"
-          width="50px"
-          height="50px"
-          fill="#0e83cd"
-          data-name="Ebene 1"
-          /> */}
+            <FacebookIcon className="finishPage__icon" />
         </a>
         <a
           href={`http://vk.com/share.php?url=loveyoucinema.com&title="Название"&description="fegrhtj"`}
           target="_blank"
           rel="noreferrer">
-          <img alt="VKontakte" src={VK} />
+          <VkIcon/>
         </a>
         <a
           href={`https://connect.ok.ru/offer?url=loveyoucinema.com&title=${message}`}
           target="_blank"
           rel="noreferrer">
-          <img alt="OK" src={OK} />
+           <OkIcon/>
         </a>
         <a
           href="https://t.me/share/url?url=loveyoucinema.com&text=Я отгадал 10 фильмов подряд"
           target="_blank"
           rel="noreferrer">
-          <img alt="Telegram" src={Telegram} />
+          <TelegramIcon/>
         </a>
         <a
           href={`whatsapp://send?text=${message}`}
           target="_blank"
           rel="noreferrer">
-          <img alt="Whatsapp" src={Whatsapp} />
+          <WhatsappIcon/>
         </a>
         <a
           href={`http://twitter.com/share?text=${message}`}
           target="_blank"
           rel="noreferrer">
-          <img alt="Twitter" src={Twitter} />
+          <TwitterIcon/>
         </a>
       </div>
       <div className="finishPage__continue">
